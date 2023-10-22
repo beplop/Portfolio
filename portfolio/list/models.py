9a5b31cb4e0c7f98fc3368208a5ac12bace11c2b
Arthur <artyr-xamidullin@mail.ru>
2023-10-22 23:57:30 +0300

    - Вывод репозиториев с гитхаба и сохранение в БД
    - Отредактировал стили: поправил левое меню

--- portfolio/list/models.py
+++ portfolio/list/models.py
@@ -2,13 +2,18 @@ from django.db import models
 
 
 class Projects(models.Model):
-    title = models.CharField('Название', max_length=50, default='')
-    anons = models.CharField('Анонс', max_length=250, default='')
-    full_text = models.TextField('Статья')
-    date = models.DateTimeField('Дата публикации')
+    name = models.CharField('Название', max_length=50, default='')
+    descript = models.CharField('Описание', max_length=250, default='', null=True)
+    date = models.DateTimeField('Дата создания')
+    language = models.CharField('Язык программирования', max_length=20, default='', null=True)
+
+    # title = models.CharField('Название', max_length=50, default='')
+    # anons = models.CharField('Анонс', max_length=250, default='')
+    # full_text = models.TextField('Статья')
+    # date = models.DateTimeField('Дата публикации')
 
     def __str__(self):
-        return self.title
+        return self.name
 
     class Meta:
         verbose_name = 'Проект'
--- portfolio/list/views.py
+++ portfolio/list/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 from .models import Projects
+from github import Github
+
+username = 'beplop'
+g = Github()
+user = g.get_user(username)
 
 
 def list_git(request):
-    gits = Projects.objects.all().order_by('title')
+    repos = user.get_repos()
+
+    for repo in repos:
+        if Projects.objects.filter(name=repo.full_name[7:]).exists():
+            pass  # надо апдейтить
+        else:
+            p = Projects(name=repo.full_name[7:], descript=repo.description, date=repo.created_at,
+                         language=repo.language)
+            p.save()
+
+    gits = Projects.objects.all().order_by('date')
     return render(request, 'list/index.html', {'gits': gits})
