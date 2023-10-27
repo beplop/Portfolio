1d98eb70d6c4cc6c28d1a81eb9e0367169a35ff8
Arthur <artyr-xamidullin@mail.ru>
2023-10-27 19:50:49 +0300

    Вывод данных в виде таблицы и добавление select (без логики)

--- portfolio/list/models.py
+++ portfolio/list/models.py
@@ -2,15 +2,12 @@ from django.db import models
 
 
 class Projects(models.Model):
+    repo_id = models.IntegerField('ID репозитория', default=0)
     name = models.CharField('Название', max_length=50, default='')
     descript = models.CharField('Описание', max_length=250, default='', null=True)
     date = models.DateTimeField('Дата создания')
     language = models.CharField('Язык программирования', max_length=20, default='', null=True)
-
-    # title = models.CharField('Название', max_length=50, default='')
-    # anons = models.CharField('Анонс', max_length=250, default='')
-    # full_text = models.TextField('Статья')
-    # date = models.DateTimeField('Дата публикации')
+    url = models.CharField('Ссылка', max_length=250, default='', null=True)
 
     def __str__(self):
         return self.name
--- portfolio/list/views.py
+++ portfolio/list/views.py
@@ -11,12 +11,22 @@ def list_git(request):
     repos = user.get_repos()
 
     for repo in repos:
-        if Projects.objects.filter(name=repo.full_name[7:]).exists():
-            pass  # надо апдейтить
+        # если в БД уже есть запись с определенным репозиторием, то обновляем ее, тем самым делая актуальной
+        if Projects.objects.filter(repo_id=repo.id).exists():
+            Projects.objects.filter(repo_id=repo.id).update(name=repo.full_name[7:], descript=repo.description,
+                                                            date=repo.created_at,
+                                                            language=repo.language, url=repo.html_url)
         else:
-            p = Projects(name=repo.full_name[7:], descript=repo.description, date=repo.created_at,
-                         language=repo.language)
+            p = Projects(repo_id=repo.id, name=repo.full_name[7:], descript=repo.description, date=repo.created_at,
+                         language=repo.language, url=repo.html_url)
             p.save()
 
-    gits = Projects.objects.all().order_by('date')
-    return render(request, 'list/index.html', {'gits': gits})
+    # gits = Projects.objects.all().order_by('date')
+    gits = Projects.objects.all()
+
+    # добавляем все используемые языки из репозиториев во множество, чтобы не было повторений
+    set_languages = set()
+    for el in gits:
+        set_languages.add(el.language)
+
+    return render(request, 'list/index.html', {'gits': gits, 'set_languages': set_languages})
