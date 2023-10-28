6ac3083f0e61ba1f66b47725f74ac0002bed0538
Arthur <artyr-xamidullin@mail.ru>
2023-10-28 03:36:11 +0300

    Исправил фильтр, сделал выпадающий список

--- portfolio/list/filters.py
+++ portfolio/list/filters.py
@@ -1,15 +1,17 @@
-from django_filters import FilterSet, ModelChoiceFilter
+from django_filters import FilterSet, ModelChoiceFilter, ChoiceFilter
 
 from .models import Projects
 
 
+def get_choices(model, field):
+    choices = []
+    for k in model.objects.values_list(field).distinct():
+        choices.append((k[0], k[0]))
+    return choices
+
+
 class ProjectsFilter(FilterSet):
-    # gits = Projects.objects.all()
-    # set_languages = set()
-    # for el in gits:
-    #     set_languages.add(el.language)
-    # language = CharFilter(lookup_expr='in')
-    # language = ModelChoiceFilter(queryset=Projects.)
+    language = ChoiceFilter(choices=get_choices(Projects, 'language'))
 
     class Meta:
         model = Projects
