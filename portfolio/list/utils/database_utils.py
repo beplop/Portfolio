21447f3217fdb7e5bae6a594b265bf76943c38a4
Arthur <artyr-xamidullin@mail.ru>
2023-11-11 01:08:37 +0300

    первичная настройка перед хостингом

--- portfolio/list/utils/database_utils.py
+++ portfolio/list/utils/database_utils.py
@@ -9,7 +9,8 @@ def diff_db_and_cache(data: list[dict]) -> list[int]:
     """
     diff_db_and_cache = list()
     cache = [x['repo_id'] for x in data]
-    for el in Projects.objects.values_list('repo_id'):
+    db = Projects.objects.values_list('repo_id')
+    for el in db:
         if el[0] not in cache:
             diff_db_and_cache.append(el[0])
     return diff_db_and_cache
